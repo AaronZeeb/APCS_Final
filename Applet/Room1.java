9b01c1b4dff48a2ac79be16e3040f723acb68eed
AaronZeeb <aaronzeeb2@gmail.com>
2017-05-12 15:07:35 -0500

    5/12

--- Applet/Room1.java
+++ Applet/Room1.java
@@ -1,36 +1,20 @@
 //Aaron Zeeb
-//Title screen for the game
-//import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.*;
 import javax.swing.*;
 import java.awt.Font;
-public class Title extends JApplet
+public class Room1 extends JApplet
 {
-    Image TitleScreen;
+    Image Room1;
     public void init() {
         //grabs the image file from the folder
-        TitleScreen=getImage(getDocumentBase(),"title.jpg");
+        Room1=getImage(getDocumentBase(),"room1.jpg");
     }
 
     public void paint(Graphics g) {
         //draws the title screen image
-        g.drawImage(TitleScreen, 0, 0, this);
-        
-        //sets color for text
-        g.setColor(Color.red);
-        //sets the font, font type, and size of font
-        g.setFont(new Font("TimesRoman", Font.BOLD, 30));
-        //draws the string with the given x,y coordinates
-        g.drawString("Title", 484, 384);
-        //sets the font, font type, and size of font
-        g.setFont(new Font("TimesRoman", Font.PLAIN, 20));
-        //draws the string with the given x,y coordinates
-        g.drawString("Created by Nick Litman, Aaron Zeeb, Alex Yang", 324, 410);
-
-        //draws the string with the given x,y coordinates
-        g.drawString("Press the Enter key to Play...", 424, 436);
-        
+        g.drawImage(Room1, 0, 0, this);
     }
 
     /**
@@ -55,7 +39,7 @@ public class Title extends JApplet
         // provide any code that needs to be run when page
         // is replaced by another page or before JApplet is destroyed 
     }
-    
+
     /**
      * Called by the browser or applet viewer to inform this JApplet that it
      * is being reclaimed and that it should destroy any resources that it
